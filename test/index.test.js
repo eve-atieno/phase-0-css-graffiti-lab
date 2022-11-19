6c1e0cad515009e8cbe0eb4a22245fa00e4cdcb5
eve-atieno <atienoevelyne53@gmail.com>
2022-11-19 12:15:31 +0300

    completed assigment

--- test/index.test.js
+++ test/index.test.js
@@ -16,19 +16,19 @@ describe("css/cleanup.css", () => {
   it("exists", () => {
     const fileExists = fs.existsSync(
       path.resolve(__dirname, "..", "css/cleanup.css")
-    );
+    )
     const hint = "The cleanup.css file must exist";
     expect(fileExists, hint).to.be.true;
-  });
+  })
 
   it("has overrides added", () => {
     const fileData = fs.statSync(
       path.resolve(__dirname, "..", "css/cleanup.css")
-    );
+    )
     const hint =
       "You should have at least 300 bytes worth of updates to css/cleanup.css";
     expect(fileData.size, hint).to.be.greaterThan(300);
-  });
+  })
 });
 
 describe("css/paint.css", () => {
